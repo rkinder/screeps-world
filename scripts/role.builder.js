8822964d5ba0a8545080a12355d97d9616aa9b76
Ryan Kinder <kinderrs@gmail.com>
2022-11-21 17:49:31 -0500

    remove visualizations

--- scripts/role.builder.js
+++ scripts/role.builder.js
@@ -5,7 +5,7 @@ var roleBuilder = {
         if(creep.store.energy < creep.store.getCapacity()) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                creep.moveTo(sources[0]);
             }
         } else {
             var targets = creep.room.find(FIND_MY_CONSTRUCTION_SITES, {
