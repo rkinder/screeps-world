16c181b2624008f6877a85fa7fb6aaf49efd121c
Ryan Kinder <kinderrs@gmail.com>
2022-11-21 17:51:18 -0500

    Remove visualizations

--- scripts/role.harvester.js
+++ scripts/role.harvester.js
@@ -5,7 +5,7 @@ var roleHarvester = {
         if(creep.store.energy < creep.store.getCapacity()) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                creep.moveTo(sources[0]);
             }
         }
         else {
@@ -18,7 +18,7 @@ var roleHarvester = {
             });
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.moveTo(targets[0]);
                 }
             }
         }
