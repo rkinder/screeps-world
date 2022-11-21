a4e5b7e27098bb1723ce7bd2ea1643ebb5e043d1
Ryan Kinder <kinderrs@gmail.com>
2022-11-21 13:48:19 -0500

    Add spawnHarvesterType

--- scripts/spawner.js
+++ scripts/spawner.js
@@ -8,16 +8,33 @@ var spawner = {
         for (var name in Game.creeps) {
             // check current role
             // check time to live
-
+            spawnRoom =spawn.room
+            curLevel = Game.rooms[spawnRoom.name].controlLevel
+            
             // if we need something, get the appropriate build
             switch(need) {
                 case 'builder':
-                    body = this.getBuilderType(spawn)
-            }
+                    this.spawnBuilderType(spawn,curLevel);
+                    break;
+                case 'harvester':
+                    this.spawnHarvesterType(spawn,curLevel);
+                    break;
+            };
         }
     },
-    getBuilderType: function(spawn) {
+    spawnBuilderType: function(spawn,controlLevel) {
         
+        body = [CARRY,CARRY,WORK,MOVE,MOVE];
+        if ((spawn.room.energyCapacityAvailable >= 350) && (spawn.room.energyCapacityAvailable <= 450)) {
+            body.push(WORK);
+        }
+        spawn.spawnCreep(body, nextName, {Memory: {role: 'builder'}});
+    },
+    spawnHarvesterType: function(spawn,controlLevel) {
+        body = [CARRY,WORK,WORK,MOVE];
+        if ((spawn.room.energyCapacityAvailable >= 350) && (spawn.room.energyCapacityAvailable <= 450)) {
+            body.push(MOVE);
+        }
     }
 
 }
