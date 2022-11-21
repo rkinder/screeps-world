608f1d31df646db7f2883cf7b378bdb358ef1cd0
Ryan Kinder <kinderrs@gmail.com>
2022-11-21 18:35:08 -0500

    Eliminated remaining tutorial code

--- scripts/main.js
+++ scripts/main.js
@@ -4,22 +4,6 @@ var spawner = require('spawner');
 
 module.exports.loop = function () {
 
-    var tower = Game.getObjectById('TOWER_ID');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
-
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
-
     for(var name in Game.spawns) {
         var spawn = Game.spawns[name];
         if (spawn.spawning == false) {
