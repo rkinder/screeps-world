b058504a792d691cd6ad7cfdcd873f147e187fa7
Ryan Kinder <kinderrs@gmail.com>
2022-11-23 11:39:48 -0500

    stores source ids in memory

--- room.manager.js
+++ room.manager.js
@@ -1,15 +1,26 @@
 var roomManager = {
 
-    storeSources: function () {
-      var spawns = Object.keys(Game.spawns); 
-      var rooms = []
+  storeSources: function () {
+    var roomSrcs = [];
 
-      for(var spawn in spawns) {
-         rooms.push(Game.spawns[spawn].room.name); 
-         name = Game.spawns[spawn].room.name;
-         sources = Game.rooms[name] 
-      } 
+     for(const spawn in Game.spawns) {
+       var name = Game.spawns[spawn].room.name;
+       var room = Game.rooms[name];
+
+     if (room.memory['sources']) {
+        var sources = room.find(FIND_SOURCES);
+        for (const src in sources) {
+          roomSrcs.push(sources[src].id);
+        }
+        room.memory['sources'] = roomSrcs;
+        console.log('added sources');
+     } else {
+        console.log("sources already mapped");
+     }
+
     } 
+  } 
+  
 }
 
 module.exports = roomManager;
